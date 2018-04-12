2058414adbb6213831489b7336b11a41e2014326
David Roth <david.m.roth@gmail.com>
2018-04-12 02:02:36 -0500

    Fixed missing prefence and updated button styling

--- js/crostini.js
+++ js/crostini.js
@@ -5,92 +5,6 @@
 'use strict';
 
 
-function CmdQueue(term) {
-  this.cmdLength = 0;
-  this.exec = term.io.sendString;
-  this.running = false
-}
-
-CmdQueue.prototype.add = function(item) {
-  var node = {item: item};
-
-  if (this.last) {
-    this.last = this.last.next = node;
-  } else {
-    this.last = this.first = node;
-  }
-
-  this.cmdLength++;
-  
-  if (!this.running) {
-    var check = ()=> {
-      if (this.cmdLength) {
-        this.exec(this.get());
-        setTimeout(check, 300);
-      }
-    }
-
-    check();
-    this.running = true;
-  }
-};
-
-CmdQueue.prototype.get = function() {
-  var node = this.first;
-  if (node) {
-    this.first = node.next;
-    if (!(--this.cmdLength)) {
-      this.last = undefined;
-      this.running = false;
-    }
-
-    return node.item;
-  }
-};
-
-function Crostini() {
-  this.init = function(term) {
-    this.term = term;
-    this.pref = term.io.terminal_.prefs_;
-    this.cmdWithDelay = new CmdQueue(term);
-
-    this.refreshUI();
-  };
-
-  this.buildCommandButtons = (toolBar, btn) => {
-    let cmdList = {
-      "container": [
-        "vmc start " + this.pref.get('chroot-image-name'),
-        "run_container.sh --container_name=" + this.pref.get('chroot-container-name') + " --user=" + this.pref.get('chroot-user-name') + " --shell"
-      ]
-    };
-
-    for (let cmdName in cmdList) {
-      btn.innerText = "Run " + cmdName;
-
-      btn.onclick = ()=> {
-        for (let cmd in cmdList[cmdName])
-          this.cmdWithDelay.add(cmdList[cmdName][cmd] + "\r");
-          document.querySelector("#terminal").focus();
-      };
-
-      toolBar.appendChild(btn);
-    }
-  };
-  
-  this.refreshUI = function(chroots) {
-    let toolbar = document.createElement("div");
-    let button = document.createElement("button");
-    toolbar.id = "toolbar";
-
-    document.querySelector("#terminal").prepend(toolbar);
-    document.querySelector("iframe").style.top = "25px";
-    document.querySelector("iframe").style.height = "calc(100% - 30px)";
-
-    this.buildCommandButtons(toolbar, button);
-  }
-}
-
 
 hterm.PreferenceManager.categoryDefinitions.push({
   id: hterm.PreferenceManager.categories.Crostini,
@@ -116,3 +30,136 @@ hterm.PreferenceManager.defaultPreferences = Object.assign(hterm.PreferenceManag
    '\n' +
    'By default, this field will be set to \'user\'.']
 });
+
+function CmdQueue(term) {
+  this.cmdLength = 0;
+  this.exec = term.io.sendString;
+  this.running = false
+  
+  this.add = function(item) {
+    var node = {item: item};
+  
+    if (this.last) {
+      this.last = this.last.next = node;
+
+    } else {
+      this.last = this.first = node;
+    }
+  
+    this.cmdLength++;
+    
+    if (!this.running) {
+      var check = ()=> {
+        if (this.cmdLength) {
+          this.exec(this.get());
+          setTimeout(check, 300);
+        }
+      }
+  
+      check();
+      this.running = true;
+    }
+  };
+  
+  this.get = function() {
+    var node = this.first;
+    if (node) {
+      this.first = node.next;
+      if (!(--this.cmdLength)) {
+        this.last = undefined;
+        this.running = false;
+      }
+  
+      return node.item;
+    }
+  };
+}
+
+function Crostini() {
+  this.init = function(term) {
+    this.term = term;
+    this.pref = term.io.terminal_.prefs_;
+    this.cmdWithDelay = new CmdQueue(term);
+    this.updateUI();
+  };
+
+  this.build_action_button = (document, items) => {
+    let body = document.querySelector('body');
+    let action_button = document.createElement('div');
+
+    action_button.className = 'action_button-wrapper';
+
+    let menu_button = document.createElement('div');
+    menu_button.className = 'action_button-menu-loader';
+    menu_button.innerHTML = '&#9776;';
+
+    for (let i = items.length - 1; i >= 0; i--) {
+      let child = document.createElement('div');
+
+      child.className = 'action_button-menu-item';
+      child.innerHTML = items[i].icon;
+
+      if (items[i].callback) {
+        child.onclick = items[i].callback
+      }
+
+      if (items[i].display) {
+        for (var key in items[i].display) {
+          if (!items[i].display.hasOwnProperty(key)) {
+            continue;
+          }
+
+          child.style[key] = items[i].display[key]
+        }
+      }
+
+      let label_wrapper = document.createElement('label');
+      label_wrapper.className = 'action_button-menu-item-label-wrapper';
+
+      let label = document.createElement('span');
+      label.className = 'action_button-menu-item-label';
+      label.innerHTML = items[i].label;
+      label_wrapper.appendChild(label);
+      child.appendChild(label_wrapper);
+      
+      action_button.appendChild(child);
+    }
+
+    action_button.appendChild(menu_button);
+    body.appendChild(action_button)
+  };
+
+  this.updateUI = function(chroots) {
+    let buildCommands = (cmdList) => {
+      let commands = [];
+    	
+      for (let idx in cmdList) {
+        let label = "Launch '" + cmdList[idx].name + "' container";
+        let command = ()=> {
+          for (let cmd in cmdList[idx].commands)
+            this.cmdWithDelay.add(cmdList[idx].commands[cmd] + "\r");
+            document.querySelector("#terminal").focus();
+        };
+    
+        commands.push({
+      		icon:'<i class="fa fa-desktop" aria-hidden="true"></i>',
+      		label: label,
+      		callback: command
+      	});
+      }
+      
+      return commands;
+    };
+
+    let cmds = buildCommands([{
+      "name": this.pref.get('chroot-container-name'),
+      "commands": [
+        "vmc start " + this.pref.get('chroot-image-name'),
+        "run_container.sh --container_name=" + this.pref.get('chroot-container-name') + " --user=" + this.pref.get('chroot-user-name') + " --shell",
+        "reset"
+      ]
+    }]);
+
+    this.build_action_button(document, cmds);
+  }
+}
