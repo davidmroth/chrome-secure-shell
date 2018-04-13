22a9e074429724aeb7a9cc896bb0ffc26cead4e1
David Roth <david.m.roth@gmail.com>
2018-04-13 09:44:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ LATEST=$(gdrive list | grep crostini | head -1 | awk '{print $1}')
 VERSION=$(cat manifest.json | grep \"version\" | awk -F: '{print $2}' | sed 's/"//g' | tr -d '[:space:]')
 
 gdrive download $LATEST
-rm -rf *
 mv *.zip /tmp/crostini.zip
 unzip /tmp/crostini.zip -d ../
+rm -rf *
 mv ../$INSTALL_DIR/* ./
 test -d ../$INSTALL_DIR && rm -r ../$INSTALL_DIR
 git checkout build.sh
